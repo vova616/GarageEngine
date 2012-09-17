afe2bc3670c399011899fe76faadd998a9d58af1
Vova <vova616@gmail.com>
2012-09-17 20:29:52 +0300

    Removed the need of Component.self
    and Component.Component() funciton

--- Engine/Component.go
+++ Engine/Component.go
@@ -1,29 +1,18 @@
 package Engine
 
-
 type BaseComponent struct {
-	self		 Component
-	hasStarted   bool
-	gameObject   *GameObject
-} 
-
-
+	hasStarted bool
+	gameObject *GameObject
+}
 
 func NewComponent() BaseComponent {
 	return BaseComponent{}
 }
 
-
-func (c *BaseComponent) onAdd(component Component, gameObject *GameObject) {	
+func (c *BaseComponent) onAdd(component Component, gameObject *GameObject) {
 	c.gameObject = gameObject
-	c.self = component
-	component.OnComponentBind(gameObject) 
+	component.OnComponentBind(gameObject)
 }
-
-func (c *BaseComponent) Component() Component {
-	return c.self
-}
-
 
 func (c *BaseComponent) GameObject() *GameObject {
 	return c.gameObject
@@ -32,7 +21,6 @@ func (c *BaseComponent) GameObject() *GameObject {
 func (c *BaseComponent) Transform() *Transform {
 	return c.gameObject.Transform()
 }
-
 
 type Component interface {
 	Draw()
@@ -45,18 +33,15 @@ type Component interface {
 	OnCollision(collision Collision)
 	OnCollisionEnter(collision Collision)
 	OnCollisionExit(collision Collision)
-    OnComponentBind(binded *GameObject)
+	OnComponentBind(binded *GameObject)
 	Destroy()
-	Started() bool
+	started() bool
 	setStarted(b bool)
 	setGameObject(gobj *GameObject)
 	onAdd(component Component, gameObject *GameObject)
-	Component() Component
-	setSelf(comp Component)
 }
 
-
-func (c *BaseComponent) Started() bool {
+func (c *BaseComponent) started() bool {
 	return c.hasStarted
 }
 
@@ -68,59 +53,50 @@ func (c *BaseComponent) setGameObject(gobj *GameObject) {
 	c.gameObject = gobj
 }
 
-func (c *BaseComponent) setSelf(comp Component) {
-	c.self = comp
-}
-
-
-
 func (c *BaseComponent) Draw() {
-	
+
 }
 
 func (c *BaseComponent) PostDraw() {
-	
+
 }
 
 func (c *BaseComponent) Update() {
-	
+
 }
 
 func (c *BaseComponent) FixedUpdate() {
-	
+
 }
 
 func (c *BaseComponent) Start() {
-	
+
 }
 
-
 func (c *BaseComponent) Clone() {
-	
+
 }
 
 func (c *BaseComponent) LateUpdate() {
-	
+
 }
 
 func (c *BaseComponent) OnCollision(collision Collision) {
-	
+
 }
 
 func (c *BaseComponent) OnCollisionEnter(collision Collision) {
-	
+
 }
 
-
 func (c *BaseComponent) OnCollisionExit(collision Collision) {
-	
+
 }
 
 func (c *BaseComponent) OnComponentBind(binded *GameObject) {
-	
+
 }
 
-
 func (c *BaseComponent) Destroy() {
-	
+
 }
