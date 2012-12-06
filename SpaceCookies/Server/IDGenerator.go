cd4a679d6db66f8db1bd6178c9c74dcf252d591a
Vova <vova616@gmail.com>
2012-12-06 20:07:46 +0200

    Made an option to make IDGen not thread safe.

--- SpaceCookies/Server/IDGenerator.go
+++ SpaceCookies/Server/IDGenerator.go
@@ -7,16 +7,20 @@ import (
 type ID int32
 
 type IDGenerator struct {
-	lastID ID
-	ids    []ID
-	locker *sync.Mutex
+	lastID     ID
+	ids        []ID
+	locker     *sync.Mutex
+	threadSafe bool
 }
 
-func NewIDGenerator(buffer int) *IDGenerator {
-	gen := &IDGenerator{0, make([]ID, 0, buffer), &sync.Mutex{}}
-	gen.locker.Lock()
+func NewIDGenerator(buffer int, threadSafe bool) *IDGenerator {
+	gen := &IDGenerator{0, make([]ID, 0, buffer), &sync.Mutex{}, threadSafe}
+	if threadSafe {
+		gen.locker.Lock()
+		defer gen.locker.Unlock()
+	}
+
 	gen.genIDs()
-	gen.locker.Unlock()
 	return gen
 }
 
@@ -30,18 +34,22 @@ func (gen *IDGenerator) genIDs() {
 func (gen *IDGenerator) NextID() ID {
 	id := ID(0)
 
-	gen.locker.Lock()
+	if gen.threadSafe {
+		gen.locker.Lock()
+		defer gen.locker.Unlock()
+	}
 	id, gen.ids = gen.ids[len(gen.ids)-1], gen.ids[:len(gen.ids)-1]
 	if len(gen.ids) == 0 {
 		gen.genIDs()
 	}
-	gen.locker.Unlock()
 
 	return id
 }
 
 func (gen *IDGenerator) PutID(id ID) {
-	gen.locker.Lock()
+	if gen.threadSafe {
+		gen.locker.Lock()
+		defer gen.locker.Unlock()
+	}
 	gen.ids = append(gen.ids, id)
-	gen.locker.Unlock()
 }
