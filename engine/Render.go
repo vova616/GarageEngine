a81751fce5b753e8eda40cbd0c155c64e88c17f2
Vova <vova616@gmail.com>
2013-02-06 12:32:42 +0200

    Added DrawSprites and camera matrix fix.
    Also cleaned Map.

--- engine/Render.go
+++ engine/Render.go
@@ -119,3 +119,64 @@ func DrawSprite(tex *Texture, uv UV, position Vector, scale Vector, rotation flo
 
 	TextureMaterial.End(nil)
 }
+
+func DrawSprites(tex *Texture, uvs []UV, positions []Vector, scales []Vector, rotations []float32, alings []AlignType, colors []Vector) {
+
+	TextureMaterial.Begin(nil)
+
+	vert := TextureMaterial.Verts
+	uvb := TextureMaterial.UV
+	mp := TextureMaterial.ProjMatrix
+	mv := TextureMaterial.ViewMatrix
+	mm := TextureMaterial.ModelMatrix
+	tx := TextureMaterial.Texture
+	ac := TextureMaterial.AddColor
+	ti := TextureMaterial.Tiling
+	of := TextureMaterial.Offset
+
+	vert.EnableArray()
+	uvb.EnableArray()
+
+	defaultBuffer.Bind(gl.ARRAY_BUFFER)
+
+	vert.AttribPointer(3, gl.FLOAT, false, 0, uintptr(0))
+	uvb.AttribPointer(2, gl.FLOAT, false, 0, uintptr(12*4))
+
+	camera := GetScene().SceneBase().Camera
+	view := camera.InvertedMatrix()
+	mv.UniformMatrix4fv(false, view)
+	mp.UniformMatrix4f(false, (*[16]float32)(camera.Projection))
+
+	tex.Bind()
+	tx.Uniform1i(0)
+
+	for i := 0; i < len(uvs); i++ {
+
+		uv, position, scale := uvs[i], positions[i], scales[i]
+
+		if !InsideScreen(uv.Ratio, position, scale) {
+			continue
+		}
+
+		rotation, aling, color := rotations[i], alings[i], colors[i]
+
+		v := Align(aling)
+		v.X *= uv.Ratio
+
+		model := Identity()
+		model.Translate(v.X, v.Y, 0)
+
+		model.Scale(scale.X*uv.Ratio, scale.Y, scale.Z)
+		model.Rotate(rotation, 0, 0, -1)
+		model.Translate(position.X, position.Y, position.Z)
+
+		mm.UniformMatrix4fv(false, model)
+		ac.Uniform4f(color.X, color.Y, color.Z, 1)
+		ti.Uniform2f(uv.U2-uv.U1, uv.V2-uv.V1)
+		of.Uniform2f(uv.U1, uv.V1)
+
+		gl.DrawArrays(gl.QUADS, 0, 4)
+	}
+
+	TextureMaterial.End(nil)
+}
--- zumbies/Map.go
+++ zumbies/Map.go
@@ -14,10 +14,15 @@ type Map struct {
 	engine.BaseComponent
 	Sprite        *engine.Sprite
 	Tiles         []uint32
-	Sprites       map[int]*engine.Sprite
-	FreeSprites   []*engine.Sprite
 	Width, Height int
 	TileSize      float32
+
+	uvs       []engine.UV
+	positions []engine.Vector
+	scales    []engine.Vector
+	rotations []float32
+	alings    []engine.AlignType
+	colors    []engine.Vector
 }
 
 func NewMap(tex *engine.Texture, uv engine.AnimatedUV) *Map {
@@ -34,8 +39,19 @@ func (m *Map) Start() {
 	tW, tH := 1000, 1000
 	m.Tiles = make([]uint32, int(tW*tH))
 
-	m.Sprites = make(map[int]*engine.Sprite)
-	m.FreeSprites = make([]*engine.Sprite, 0, int(w*h))
+	m.uvs = make([]engine.UV, int(w*h))
+	m.positions = make([]engine.Vector, int(w*h))
+	m.scales = make([]engine.Vector, int(w*h))
+	m.rotations = make([]float32, int(w*h))
+	m.colors = make([]engine.Vector, int(w*h))
+	m.alings = make([]engine.AlignType, int(w*h))
+
+	for i, _ := range m.alings {
+		m.alings[i] = engine.AlignCenter
+	}
+	for i, _ := range m.colors {
+		m.colors[i] = engine.One
+	}
 
 	for i, _ := range m.Tiles {
 		m.Tiles[i] = rand.Uint32() % 2 //uint32(len(m.Sprite.UVs))
@@ -70,104 +86,6 @@ func (m *Map) Start() {
 	*/
 	m.Width = int(tW)
 	m.Height = int(tH)
-	/*
-		for index := uint32(0); index < uint32(cap(m.FreeSprites)); index++ {
-			tile := engine.NewGameObject("Tile")
-			tile.Transform().SetParent2(m.GameObject())
-			tile.Transform().SetScalef(1.005, 1.005)
-
-			tile.Transform().SetParent2(nil)
-			if 1 == 1 {
-				pos := tile.Transform().Position()
-				tile.Transform().SetPositionf(pos.X+0.5, pos.Y+0.5)
-				engine.GetScene().SceneBase().AddGameObject(tile)
-				tile.SetActive(false)
-			}
-
-			var s *engine.Sprite = new(engine.Sprite)
-			*s = *m.Sprite
-			s.SetAnimationIndex(0)
-			s.AnimationSpeed = 0
-			tile.GameObject().AddComponent(s)
-
-			m.FreeSprites = append(m.FreeSprites, s)
-		}
-	*/
-}
-
-func (m *Map) LateUpdate() {
-	return
-	camera := engine.GetScene().SceneBase().Camera
-	cameraPos := camera.Transform().WorldPosition()
-	mapPos := m.Transform().WorldPosition()
-
-	for i, s := range m.Sprites {
-		tPos := s.Transform().WorldPosition()
-		if tPos.X > cameraPos.X+float32(engine.Width)+m.TileSize || tPos.X < cameraPos.X-m.TileSize {
-			m.FreeSprites = append(m.FreeSprites, s)
-			//s.Transform().SetParent(nil)
-			s.GameObject().SetActive(false)
-			delete(m.Sprites, i)
-			continue
-		}
-		if tPos.Y > cameraPos.Y+float32(engine.Height)+m.TileSize || tPos.Y < cameraPos.Y-m.TileSize {
-			m.FreeSprites = append(m.FreeSprites, s)
-			//s.Transform().SetParent(nil)
-			s.GameObject().SetActive(false)
-			delete(m.Sprites, i)
-			continue
-		}
-
-	}
-
-	width := int(float32(engine.Width)/m.TileSize) + 2
-	height := int(float32(engine.Height)/m.TileSize) + 2
-
-	camX := (int(cameraPos.X) / int(m.TileSize)) * int(m.TileSize)
-	camY := (int(cameraPos.Y) / int(m.TileSize)) * int(m.TileSize)
-
-	for x := -1; x < width; x++ {
-		for y := -1; y < height; y++ {
-			var tilePos engine.Vector
-			tilePos.X = float32(camX + (x * int(m.TileSize)))
-			tilePos.Y = float32(camY + (y * int(m.TileSize)))
-
-			if tilePos.X > cameraPos.X+float32(engine.Width)+m.TileSize || tilePos.X < cameraPos.X-m.TileSize {
-				continue
-			}
-			if tilePos.Y > cameraPos.Y+float32(engine.Height)+m.TileSize || tilePos.Y < cameraPos.Y-m.TileSize {
-				continue
-			}
-
-			pos := tilePos.X + (tilePos.Y * float32(width))
-			s, exists := m.Sprites[int(pos)]
-			if exists {
-				continue
-			}
-
-			tx := (int(tilePos.X-mapPos.X) / int(m.TileSize)) + (m.Width / 2)
-			ty := (int(tilePos.Y-mapPos.Y) / int(m.TileSize)) + (m.Height / 2)
-			if tx < 0 || ty < 0 || tx >= m.Width || ty >= m.Height {
-				continue
-			}
-
-			tileIndex := (tx) + (ty * m.Width)
-			if tileIndex >= len(m.Tiles) {
-				continue
-			}
-
-			if len(m.FreeSprites) == 0 {
-				panic("Not enough sprites")
-			}
-
-			s, m.FreeSprites = m.FreeSprites[len(m.FreeSprites)-1], m.FreeSprites[:len(m.FreeSprites)-1]
-			m.Sprites[int(pos)] = s
-			s.GameObject().SetActive(true)
-			s.Transform().SetWorldPositionf(tilePos.X, tilePos.Y)
-			s.Transform().SetScalef(m.TileSize+0.000, m.TileSize+0.000)
-			s.SetAnimationIndex(int(m.Tiles[tileIndex]))
-		}
-	}
 }
 
 func (m *Map) Draw() {
@@ -181,6 +99,8 @@ func (m *Map) Draw() {
 	camX := (int(cameraPos.X) / int(m.TileSize)) * int(m.TileSize)
 	camY := (int(cameraPos.Y) / int(m.TileSize)) * int(m.TileSize)
 
+	index := 0
+
 	for x := -1; x < width; x++ {
 		for y := -1; y < height; y++ {
 			var tilePos engine.Vector
@@ -205,7 +125,14 @@ func (m *Map) Draw() {
 				continue
 			}
 
-			engine.DrawSprite(m.Sprite.Texture, m.Sprite.UVs[int(m.Tiles[tileIndex])], tilePos, engine.NewVector2(m.TileSize, m.TileSize), 0, engine.AlignCenter, engine.One)
+			m.uvs[index] = m.Sprite.UVs[int(m.Tiles[tileIndex])]
+			m.positions[index] = tilePos
+			m.scales[index] = engine.NewVector2(m.TileSize, m.TileSize)
+			//engine.DrawSprite(m.Sprite.Texture, m.Sprite.UVs[int(m.Tiles[tileIndex])], tilePos, engine.NewVector2(m.TileSize, m.TileSize), 0, engine.AlignCenter, engine.One)
+			index++
 		}
 	}
+
+	engine.DrawSprites(m.Sprite.Texture, m.uvs[:index], m.positions[:index], m.scales[:index], m.rotations[:index], m.alings[:index], m.colors[:index])
+
 }
