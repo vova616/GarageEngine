890ba0b068b05b4fa52d7f37775bbcd596676eb9
Vova <vova616@gmail.com>
2013-03-07 14:05:48 +0200

    DepthMap should be faster now.

--- engine/DepthMap.go
+++ engine/DepthMap.go
@@ -4,23 +4,19 @@ var depthMap = make(map[int8][]*GameObject)
 
 func depthMapAdd(depth int8, object *GameObject) (index int) {
 	arr := depthMap[depth]
-	for i, obj := range arr {
-		if obj == nil {
-			index = i
-			arr[i] = object
-			return
-		}
-	}
 	arr = append(arr, object)
 	depthMap[depth] = arr
 	return len(arr) - 1
 }
 
-func depthMapRemove(depth int8, index int) {
+func depthMapRemove(depth int8, index int) (deleted bool) {
 	if index >= 0 {
 		arr := depthMap[depth]
-		if index < len(arr) {
-			arr[index] = nil
+		if index > len(arr)-1 {
+			return false
 		}
+		arr[len(arr)-1], arr[index], depthMap[depth] = nil, arr[len(arr)-1], arr[:len(arr)-1]
+		return true
 	}
+	return false
 }
