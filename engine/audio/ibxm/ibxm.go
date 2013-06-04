640bd6827cf62b4cc49872cb402d996ab877b038
Vovchik <vova616@gmail.com>
2013-06-05 01:27:25 +0300

    Added wav reader and updated ibxm.

--- engine/audio/ibxm/ibxm.go
+++ engine/audio/ibxm/ibxm.go
@@ -6,9 +6,13 @@ import (
 	"os"
 )
 
+var test = audio.AudioClip(&IBXM{})
+
 type IBXM struct {
 	ibxm        *ibxmgo.IBXM
 	audioBuffer []int32
+	format      audio.Format
+	length      int
 }
 
 func NewClip(path string) (*IBXM, error) {
@@ -25,44 +29,64 @@ func NewClip(path string) (*IBXM, error) {
 	if e != nil {
 		return nil, e
 	}
-	return &IBXM{clip, nil}, nil
+
+	return &IBXM{clip, nil, audio.Stereo16, clip.Length() * 2}, nil
 }
 
 /* Returns the length of the buffer required by NextBuffer(). */
 func (this *IBXM) BufferLength() int {
-	return (this.ibxm.CalculateTickLen(32, 128000) + 65) * 2
+	return this.ibxm.AudioBufferLength()
 }
 
-func (this *IBXM) NextBuffer(outputBuf []int16) (samples int) {
+func (this *IBXM) SetPosition(pos int) {
+	this.ibxm.SetSequencePos(pos)
+}
+
+func (this *IBXM) NextBuffer(outputBuf []int16, mono bool) (samples int) {
 	if this.audioBuffer == nil {
 		this.audioBuffer = make([]int32, this.ibxm.AudioBufferLength())
 	}
 
 	n, _ := this.ibxm.GetAudio(this.audioBuffer)
-	n *= 2
-	for i := 0; i < n; i++ {
-		x := this.audioBuffer[i]
-		if x > 32767 {
-			x = 32767
-		} else if x < -32768 {
-			x = -32768
+	if mono {
+		for i := 0; i < n; i++ {
+			x := int((float64(this.audioBuffer[i*2]) + float64(this.audioBuffer[i*2+1])) / 2)
+			if x > 32767 {
+				x = 32767
+			} else if x < -32768 {
+				x = -32768
+			}
+			outputBuf[i] = int16(x)
 		}
-		outputBuf[i] = int16(x)
+		//n *= 2
+		this.format = audio.Mono16
+	} else {
+		n *= 2
+		for i := 0; i < n; i++ {
+			x := this.audioBuffer[i]
+			if x > 32767 {
+				x = 32767
+			} else if x < -32768 {
+				x = -32768
+			}
+			outputBuf[i] = int16(x)
+		}
+		this.format = audio.Stereo16
 	}
 	return n
 }
 
 func (this *IBXM) Clone() (audio.AudioClip, error) {
 	ib, e := ibxmgo.NewIBXM(this.ibxm.Module(), this.ibxm.SampleRate())
-	return &IBXM{ib, nil}, e
+	return &IBXM{ib, nil, this.format, this.length}, e
 }
 
 func (this *IBXM) AudioFormat() audio.Format {
-	return audio.Stereo16
+	return this.format
 }
 
 func (this *IBXM) Length() int {
-	return this.ibxm.Length()
+	return this.length
 }
 
 func (this *IBXM) SampleRate() int {
---
+++ engine/audio/wav/wav.go
@@ -0,0 +1,159 @@
+package wav
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/vova616/GarageEngine/engine/audio"
+	"io"
+	"os"
+)
+
+var test = audio.AudioClip(&Wav{})
+
+type Wav struct {
+	Format
+	Data        []int16
+	index       int
+	audioFormat audio.Format
+}
+
+func (this *Wav) BufferLength() int {
+	return int(this.Format.SampleRate) / 2
+}
+
+func (this *Wav) NextBuffer(buff []int16, mono bool) int {
+	if mono && this.audioFormat != audio.Mono16 {
+		panic("does not support stereo to mono conversion")
+	}
+
+	n := copy(buff, this.Data[this.index:])
+	this.index += n
+	if this.index >= len(this.Data) {
+		this.index = 0
+	}
+	return n
+}
+
+func (this *Wav) Clone() (audio.AudioClip, error) {
+	return &Wav{this.Format, this.Data, 0, this.audioFormat}, nil
+}
+
+func (this *Wav) SetPosition(pos int) {
+	this.index = pos
+}
+
+func (this *Wav) Length() int {
+	return len(this.Data)
+}
+
+func (this *Wav) SampleRate() int {
+	return int(this.Format.SampleRate)
+}
+func (this *Wav) AudioFormat() audio.Format {
+	return this.audioFormat
+}
+
+type Format struct {
+	FormatTag     int16
+	Channels      int16
+	SampleRate    int32
+	AvgBytes      int32
+	BlockAlign    int16
+	BitsPerSample int16
+}
+
+type Format2 struct {
+	Format
+	SizeOfExtension int16
+}
+
+type Format3 struct {
+	Format2
+	ValidBitsPerSample int16
+	ChannelMask        int32
+	SubFormat          [16]byte
+}
+
+func NewClip(path string) (*Wav, error) {
+	mr, e := os.Open(path)
+	if e != nil {
+		return nil, e
+	}
+	clip, e := ReadWavHeader(mr)
+	if e != nil {
+		return nil, e
+	}
+	return clip, nil
+}
+
+func ReadWavHeader(reader io.Reader) (*Wav, error) {
+	var buff [4]byte
+	reader.Read(buff[:4])
+
+	if string(buff[:4]) != "RIFF" {
+		return nil, fmt.Errorf("Not a WAV file.\n")
+	}
+
+	var size int32
+	binary.Read(reader, binary.LittleEndian, &size)
+
+	reader.Read(buff[:4])
+
+	if string(buff[:4]) != "WAVE" {
+		return nil, fmt.Errorf("Not a WAV file.\n")
+	}
+
+	reader.Read(buff[:4])
+
+	if string(buff[:4]) != "fmt " {
+		return nil, fmt.Errorf("Not a WAV file.\n")
+	}
+
+	binary.Read(reader, binary.LittleEndian, &size)
+
+	var format Format
+
+	switch size {
+	case 16:
+		binary.Read(reader, binary.LittleEndian, &format)
+	case 18:
+		var f2 Format2
+		binary.Read(reader, binary.LittleEndian, &f2)
+		format = f2.Format
+	case 40:
+		var f3 Format3
+		binary.Read(reader, binary.LittleEndian, &f3)
+		format = f3.Format
+	}
+
+	reader.Read(buff[:4])
+
+	if string(buff[:4]) != "data" {
+		return nil, fmt.Errorf("Not supported WAV file.\n")
+	}
+
+	binary.Read(reader, binary.LittleEndian, &size)
+
+	wavData := make([]byte, size)
+	n, e := reader.Read(wavData)
+	if e != nil {
+		return nil, fmt.Errorf("Cannot read WAV data.\n")
+	}
+	if int32(n) != size {
+		return nil, fmt.Errorf("WAV data size doesnt match.\n")
+	}
+
+	wavInts := make([]int16, size/2)
+	for i := 0; i < len(wavInts); i++ {
+		wavInts[i] = int16(binary.LittleEndian.Uint16(wavData[i*2:]))
+	}
+
+	af := audio.Mono8
+	if format.Channels == 1 {
+		af = audio.Mono16
+	} else {
+		af = audio.Stereo16
+	}
+
+	return &Wav{format, wavInts, 0, af}, nil
+}
